ad8ffda23f9626866257797d3f515d8d018bb168
PolledKenneth <polledkenneth031@gmail.com>
2025-05-04 14:37:53 +0800

    v1

---
+++ service-worker.js
@@ -0,0 +1,44 @@
+const CACHE_NAME = "cockroach-shooter-cache-v1";
+const urlsToCache = [
+  "./",
+  "./index.html",
+  "./style.css",
+  "./script.js",
+  "./manifest.json",
+  "./assets/img/scope.png",
+  "./assets/img/cockroach.png",
+  "./assets/img/dead-cockroach.png",
+  "./assets/msc/background-music.mp3",
+  "./assets/msc/kill-sound.mp3"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
+
+self.addEventListener("activate", (event) => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then((cacheNames) =>
+      Promise.all(
+        cacheNames.map((cacheName) => {
+          if (!cacheWhitelist.includes(cacheName)) {
+            return caches.delete(cacheName);
+          }
+        })
+      )
+    )
+  );
+});
